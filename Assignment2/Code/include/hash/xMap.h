e2543eda80a50d5278326df411806bfca8a120ab
GaLuoc05 <167160538+GaLuoc05@users.noreply.github.com>
2024-10-24 00:03:31 +0700

    123

--- Assignment2/Code/include/hash/xMap.h
+++ Assignment2/Code/include/hash/xMap.h
@@ -248,7 +248,7 @@ V xMap<K,V>::put(K key, V value){
     DLinkedList list = table[index];
     for (DLinkedList::Iterator it = list.begin(); it != list.end(); it++){
         if (keyEqual != NULL) {
-            if (keyEqual(*it.key, key)) 
+            if (keyEqual(*it.key, key)) return
         }
     }
 
