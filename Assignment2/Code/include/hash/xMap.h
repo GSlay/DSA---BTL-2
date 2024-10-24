7294fb5453a5a89a1f82b2c4d48618b91d1e11a9
GSlay <mha220871@gmail.com>
2024-10-24 10:13:35 +0700

    implement_3 update: V xMap<K,V>::put(K key, V value)

--- Assignment2/Code/include/hash/xMap.h
+++ Assignment2/Code/include/hash/xMap.h
@@ -246,12 +246,17 @@ V xMap<K,V>::put(K key, V value){
     V retValue = value;
     //YOUR CODE IS HERE    
     DLinkedList list = table[index];
-    for (DLinkedList::Iterator it = list.begin(); it != list.end(); it++){
-        if (keyEqual != NULL) {
-            if (keyEqual(*it.key, key)) 
+    if (keyEqual != NULL) {
+        for (DLinkedList::Iterator it = list.begin(); it != list.end(); it++){
+            if (keyEqual(*it.key, key)) *it.value = value;
+            else list.add(Entry(key, value));
+        }
+    } else {
+        for (DLinkedList::Iterator it = list.begin(); it != list.end(); it++){
+            if (*it.key == key) *it.value = value;
+            else list.add(Entry(key, value));
         }
     }
-
     return retValue;
 }
 
