ea749994b7441e1655ae04863d8464789665af26
GSlay <mha220871@gmail.com>
2024-11-13 13:41:42 +0700

    implement dataset.h

--- Assignment2/Code/include/loader/dataset.h
+++ Assignment2/Code/include/loader/dataset.h
@@ -74,6 +74,22 @@ public:
     TensorDataset(xt::xarray<DType> data, xt::xarray<LType> label){
         /* TODO: your code is here for the initialization
          */
+        this->data = data;
+        this->label = label;
+        // chuyển đổi shape của data và label sang xt::svector<unsigned long>
+        auto data_shape_tmp = data.shape();
+        auto label_shape_tmp = label.shape();
+
+        data_shape.resize(data_shape_tmp.size());
+        label_shape.resize(label_shape_tmp.size());
+
+        for (unsigned long i = 0; i < (unsigned long)data_shape_tmp.size(); ++i) {
+            data_shape[i] = (unsigned long)(data_shape_tmp[i]);
+        }
+        
+        for (unsigned long i = 0; i < (unsigned long)label_shape_tmp.size(); ++i) {
+            label_shape[i] = (unsigned long)(label_shape_tmp[i]);
+        }
     }
     /* len():
      *  return the size of dimension 0
@@ -81,7 +97,7 @@ public:
     int len(){
         /* TODO: your code is here to return the dataset's length
          */
-        return 0; //remove it when complete
+        return data_shape[0]; //remove it when complete
     }
     
     /* getitem:
@@ -90,15 +106,23 @@ public:
     DataLabel<DType, LType> getitem(int index){
         /* TODO: your code is here
          */
+        xt::xarray<DType> data_item = xt::view(data, index, xt::all());
+        xt::xarray<LType> label_item;
+        if (label_shape.size() > 0)
+            label_item = xt::view(label, index, xt::all());
+        else label_item = 0;
+        return DataLabel<DType, LType>(data_item, label_item);
     }
     
     xt::svector<unsigned long> get_data_shape(){
         /* TODO: your code is here to return data_shape
          */
+        return data_shape;
     }
     xt::svector<unsigned long> get_label_shape(){
         /* TODO: your code is here to return label_shape
          */
+        return label_shape;
     }
 };
 
