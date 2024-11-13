1f513354c15f6a212f956be6175d8c16fc323f90
GSlay <mha220871@gmail.com>
2024-11-13 10:45:39 +0700

    implement CrossEntropy.cpp

--- Assignment2/Code/src/ann/loss/CrossEntropy.cpp
+++ Assignment2/Code/src/ann/loss/CrossEntropy.cpp
@@ -26,7 +26,23 @@ CrossEntropy::~CrossEntropy() {
 
 double CrossEntropy::forward(xt::xarray<double> X, xt::xarray<double> t){
     //YOUR CODE IS HERE
+    // Y: Dự đoán của mô hình, T: Nhãn thực tế (soft-label hoặc hard-label)
+    
+    // Tính toán Cross-Entropy cho từng mẫu
+    xt::xarray<double> ce = -t * xt::log(X);  // EPSILON tránh chia cho 0, đảm bảo không bị log(0)
+
+    // Tổng hoặc trung bình các giá trị Cross-Entropy (reduce_mean=true là trung bình, false là tổng)
+    double loss = m_eReduction ? xt::sum(ce)() / X.shape(0) : xt::sum(ce)();
+
+    return loss;
 }
 xt::xarray<double> CrossEntropy::backward() {
     //YOUR CODE IS HERE
+    const int EPSILON = 10e-7;
+    int N_norm = m_eReduction ? 1 : m_aCached_Ypred.shape(0);
+    // Tính gradient của hàm Cross-Entropy theo công thức (28)
+    xt::xarray<double> dY =  - 1/N_norm * (m_aYtarget / (m_aCached_Ypred + EPSILON));  // Đảm bảo không chia cho 0 nhờ EPSILON
+
+    // Trả về gradient dY
+    return dY;
 }
